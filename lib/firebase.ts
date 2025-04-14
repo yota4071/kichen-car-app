f500709a7a8255fe554d945411195d373dc3bdf8
小澤瑶太 <ozawayota@MacBook-Air2024.local>
2025-04-14 23:34:52 +0900

    セキュリティ

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-export const auth = getAuth(app);
+export const auth = getAuth(app); 
 export const provider = new GoogleAuthProvider();
 
 // 以下、型定義を追加
