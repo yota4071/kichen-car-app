aaf537df8f528963e040c175442e83b568d22168
BB-KING777 <syutaryosuke@gmail.com>
2025-04-15 20:23:19 +0900

    スマホ版デザイン修正1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import '@/styles/menu-styles.css';
 import '@/styles/header.module.css'; // 追加
 import '@/styles/map-styles.css'; // 追加
 import '@/styles/contact.css'; // 追加
+import '@/styles/responsive-fixes.css'; // 追加：スマホ版のレイアウト修正
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
