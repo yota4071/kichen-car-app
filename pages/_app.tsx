432f6a2bec2bdc6e88421470bb4b5e4470dd1d8d
BB-KING777 <syutaryosuke@gmail.com>
2025-05-11 17:07:22 +0900

    css改修3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ import '@/styles/header.module.css'; // 追加
 import '@/styles/map-styles.css'; // 追加
 import '@/styles/contact.css'; // 追加
 import '@/styles/responsive-fixes.css'; // 追加：スマホ版のレイアウト修正
+import '@/styles/override.css';
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
