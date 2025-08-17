f237a84663f331500872b741c5604fa5d0e922db
BB-KING777 <syutaryosuke@gmail.com>
2025-08-17 21:39:08 +0900

    Googleのあれ追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WJSJJ07CND"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WJSJJ07CND');
+            `,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet" />
